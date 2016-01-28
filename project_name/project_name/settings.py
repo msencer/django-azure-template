d1b2ddfe4cb1b6f23f3347f72b7d2e737eaa5c5d
Mehmet Sencer KARADAYI <sencer@karadayi.net>
2016-01-28 23:02:11 +0200

    deleted outdated setting of template_dir

--- project_name/project_name/settings.py
+++ project_name/project_name/settings.py
@@ -65,13 +65,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = '{{ project_name }}.wsgi.application'
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
-
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
